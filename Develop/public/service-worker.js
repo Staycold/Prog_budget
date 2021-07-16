96181b71b89929cad58d363d6cb4d1c258bfe93b
patrick brodie <patrickxbrodie@hotmail.com>
2021-07-16 15:19:54 -0400

    fixed the script in html

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,9 +1,9 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/index.js",
+    "index.js",
     "/db.js",
-    "/css/styles.css",
+    "/styles.css",
     "/manifest.webmanifest",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
